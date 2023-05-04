6799a724e40c73150dc0fea44d948de434daf729
Povlean <104959958+Povlean@users.noreply.github.com>
2023-05-04 21:01:19 +0800

    Delete management-backend directory

--- management-backend/src/main/java/com/ean/gundam/constants/CommonConstant.java
+++
@@ -1,5 +0,0 @@
-package com.ean.gundam.constants;
-
-public interface CommonConstant {
-    String USER_LOGIN_STATE = "login_state";
-}
--- management-backend/src/main/java/com/ean/gundam/constants/ResCode.java
+++
@@ -1,8 +0,0 @@
-package com.ean.gundam.constants;
-
-public interface ResCode {
-    Integer SUCCESS = 20000;
-    Integer NOT_LOGIN = 301;
-    Integer ERROR = 20001;
-    Integer NO_AUTH = 300;
-}
